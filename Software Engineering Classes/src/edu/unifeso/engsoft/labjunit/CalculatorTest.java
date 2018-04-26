83006953e93186e5530c77cfa96dc3863a78357a
Lucas de Andrade <lucas.andrade@unifeso.edu.br>
2018-04-26 07:38:35 -0300

    CalculatorTest Exercise

---
+++ Software Engineering Classes/src/edu/unifeso/engsoft/labjunit/CalculatorTest.java
@@ -0,0 +1,14 @@
+package edu.unifeso.engsoft.labjunit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testEvaluate() {
+		fail("Not yet implemented");
+	}
+
+}
